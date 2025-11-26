e29f4cb38924bc6e539c79621da922feec17677b
Rahvokkendov <rahvokkendov@gmail.com>
2025-11-27 01:33:51 +0700

    MainMenu And BGM Added

---
+++ Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+    [field: SerializeField] protected AudioSource clickPop;
+    public void ButtonStart()
+    {
+        SceneManager.LoadScene("MainScene");
+        clickPop.Play();
+    }
+
+    public void ButtonQuit() 
+    { 
+        Application.Quit();
+        clickPop.Play();
+    }
+}
